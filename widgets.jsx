1c0531335e520347be9323efd00cecfacdb7ab67
App Academy Student <student@appacademy.io>
2017-07-07 11:45:39 -0700

    Setup react widget project

---
+++ widgets.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Clock from './clock';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Clock />, document.getElementById('root'));
+});
